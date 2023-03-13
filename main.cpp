616c6d872fcd7d77d1074e5827a0cbd36d721185
margo <f411inv41@gmail.com>
2023-03-13 23:24:39 +0200

    idk

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 #include "olcPixelGameEngine.h"
 #include "masspoint.hpp"
+#include "spring.hpp"
 
 class JELLY : public olc::PixelGameEngine
 {
@@ -16,10 +17,31 @@ public:
 
 	MassPoint* point = nullptr;
 
+	olc::vf2d springPos1 = { 240.0, 260.0 };
+	olc::vf2d springPos2 = { 270.0, 260.0 };
+	olc::vf2d springPos3 = { 300.0, 260.0 };
+
+
+	olc::vf2d springPos1 = { 240.0, 260.0 };
+	olc::vf2d springPos2 = { 270.0, 260.0 };
+	olc::vf2d springPos3 = { 300.0, 260.0 };
+
+
 	bool OnUserCreate() override
 	{
 		point = new MassPoint(this, pointPos, 1.0, 1.0, 3.0);
 
+		Spring *springs[4];
+
+		springs[0] = new Spring(this, springPos1, 0.98, 8.0, 0.1, 0);
+		springs[1] = new Spring(this, springPos2, 0.98, 8.0, 0.1, 1);
+		springs[3] = new Spring(this, springPos3, 0.98, 8.0, 0.1, 2);
+
+		void sprUpdate(*Spring) {
+			for (int i = 0; i < 4; i++) {
+				springs[i]->FINALupdate();
+			}
+		}
 		return true;
 	}
 
@@ -33,7 +55,11 @@ public:
 		//point->force = gravity; //( * spring force)
 		//point->velocity = (point->force / point->mass) * deltaTime;
 
-		point->draw();
+		sprUpdate(&Spring);
+
+		sprUpdate(&Spring);
+
+		point->update(deltaTime);
 		return true;
 	}
 };
